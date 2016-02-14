5663ce3b61b151a5fb869dd8515bf748c3e65bf4
RomarioVice <RomarioBrio@gmail.com>
2016-02-14 15:25:00 +0300

    10 labs

--- 9.py
+++ 9.py
@@ -8,7 +8,7 @@ N = random.randint(1, 1000)
 SP = [random.randint(1,9999) for i in range(N)]
 ST1 = math.log(N,2)
 if 2 ** ST1 == N: 
-    ST2 = ST1
+    ST2 = int(ST1)
 else: 
     ST2 = int(ST1 + 1)
 SP2 = int((2 ** ST2) - N)
@@ -16,10 +16,10 @@ SP = SP + [0] * SP2
 
 #printing some information 
 
-print SP
+print '\nCreated Array:\n', '\n', SP
 print '\nSome Information:'
 print '\nThe Amount of Numbers: ', N
-print '\nFirst Exponent of 2:', ST1
-print '\n2 in N = ', N + SP2
-print '\nSecond Exponent of 2:', ST2
-print '\nThe Number of Zeros: ', SP2
+print '\nFirst Extent of 2:', ST1
+print '\nThe Nearest Extent of 2:', ST2
+print '\nThe Amount of Numbers in the Nearest Extent of 2:', N + SP2
+print '\nThe Number of Zeros:', SP2
